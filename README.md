5c814be69f676c5a56d6d5dbad715dbd814f6406
Tay Jovan <jovantay521@gmail.com>
2024-06-19 09:44:51 +0800

    updated

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
 # llamachain
-RAG application using langchain and llama3
+## Description
+Retreival Augmented Generation (RAG) application using langchain and llama3
+
+## Getting Started
+### Pre-requisites
+- [ollama](https://ollama.com/download)
+- [llama3](https://ollama.com/library/llama3)
+- [nomic-embed-text](https://ollama.com/library/nomic-embed-text)
+
+### Installation
+Clone this repository
+```sh
+git clone https://github.com/jovantay521/llamachain.git
+```
+
+Install Python 3.12.x ([pyenv installation](https://github.com/pyenv/pyenv?tab=readme-ov-file#installation))
+```sh
+pyenv install 3.12
+```
+```sh
+cd piggy
+pyenv local 3.12
+```
+Set up virtual environment
+```sh
+python -m venv .venv
+source .venv/bin/activate
+```
+Install required packages
+```sh
+pip install -r requirements.txt
+```
+
+### Updating environment variables
+#### Required
+Sign up for a [langsmith account](https://smith.langchain.com/) and generate an API key  
+
+Replace the environment variable in main.py
+```py
+os.environ["LANGCHAIN_API_KEY"] = "..."
+```
+
+#### Optional
+If LLM Model is on a different host, update the base url
+```python
+LLM = ChatOllama(base_url="...", model="llama3")
+```
+
+If using a different LLM model(e.g. llama3:70b), update the model variable
+```py
+LLM = ChatOllama(model="llama3:70b")
+```
+
+If your system has spare cpu threads, it is recommended to allocate them to ollama for faster generation of results
+```py
+LLM = ChatOllama(model="llama3", num_thread="...")
+```
+
+If using a different [embedding model](https://python.langchain.com/v0.2/docs/how_to/#embedding-models)
+```py
+oembed = OllamaEmbedding(model="...")
+```
+
+## Usage
+main.py parses documents into the LLM which will be used to generate a response
+
+Upload relevant documents to the documents directory
+```sh
+mv /path/to/documents ./documents
+```
+
+Run the program
+```sh
+python main.py
+```
+
+## Built using
+- LLM: ollama, llama3, nomic-embed-text
+- Document Loader: PyMuPDFReader, RapidOCR 
+- Vectorstore: ChromaDB
+- Langchain/Langsmith
+
+## Credits
+- Jovan (@jovantay521)
+- Ziyang (@Zycranny)
---
+++ main.py
@@ -0,0 +1,69 @@
+# imports
+import os
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.chat_models import ChatOllama
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+
+# environment variables
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = "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"
+os.environ["LANGCHAIN_PROJECT"] = "llamachain"
+
+# llm
+LLM = ChatOllama(base_url="http://192.168.8.3:11434", model="llama3:70b", num_thread=96)
+# LLM = ChatOllama(model="llama3b")
+
+all_splits = []
+directory_path = "documents"
+
+for filename in os.listdir(directory_path):
+    file_path = os.path.join(directory_path, filename)
+    
+    if os.path.isfile(file_path):
+        try:
+            print(f"=== loading document: {file_path} ===")
+            # 1. Load document
+            loader = PyMuPDFLoader(file_path, extract_images=True)
+            docs = loader.load()
+
+            # 2. Split document
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+            all_splits.extend(text_splitter.split_documents(docs))
+
+        except Exception as e:
+            print(f"Error processing file {file_path}: {e}")
+
+# 3. store documents
+print("=== storing documents ===")
+oembed = OllamaEmbeddings(base_url="http://192.168.8.3:11434", model="nomic-embed-text", num_thread=96)
+# oembed = OllamaEmbeddings(model="nomic-embed-text")
+vectorstore = Chroma.from_documents(documents=all_splits, embedding=oembed)
+
+# 4. retrieve documents
+retriever = vectorstore.as_retriever(search_type="similarity")
+
+system_prompt = (
+    "You are a helpful assistant. Answer my questions to your best ability and keep the responses to 500 words"
+    "\n\n"
+    "{context}"
+)
+
+final_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+# 5. RAG chain
+question_answer_chain = create_stuff_documents_chain(LLM, final_prompt)
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+print("=== generating output ===")
+results = rag_chain.invoke({"input": "What is eternalblue"})
+print(results['answer'])
